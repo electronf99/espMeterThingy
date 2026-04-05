0623c0ef18142adfcf4fd7ac3667af83c7b26209
David Peters <davidp@electronf.com>
2026-04-05 13:35:34 +1000

    i2c testing

--- tests/scani2c.py
+++ tests/scani2c.py
@@ -1,8 +1,9 @@
 # I2C Scanner MicroPython
 from machine import Pin, SoftI2C
+from pico_i2c_lcd import I2cLcd
 
 # You can choose any other combination of I2C pins
-i2c = SoftI2C(scl=Pin(15), sda=Pin(16))
+i2c = SoftI2C(scl=Pin(8), sda=Pin(7), freq=500_000)
 
 print('I2C SCANNER')
 devices = i2c.scan()
@@ -14,3 +15,6 @@ else:
 
   for device in devices:
     print("I2C hexadecimal address: ", hex(device))
+
+lcd = I2cLcd(i2c, 0x27, 2, 16)
+lcd.putstr("Hello World")
